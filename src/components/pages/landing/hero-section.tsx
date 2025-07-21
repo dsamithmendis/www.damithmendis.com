37ca3898464a8731b07701ac0819386135453557
dsamithmendis <samithmendis.01@gmail.com>
2025-07-21 16:02:07 +0530

    feat: test-one

--- src/components/pages/landing/hero-section.tsx
+++ src/components/pages/landing/hero-section.tsx
@@ -3,22 +3,22 @@
 import Image from "next/image";
 import { BackgroundGradient } from "@/components/ui/background-gradient";
 import { FlipWords } from "@/components/ui/flip-words";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+import { HeroMainWords, HeroFlipWords, HeroLinks } from "@/components/lib/hero-section";
 
 export default function HeroSection() {
-  const words = ["Filmmaker", "Lecturer"];
-
   return (
     <section className="bg-black text-white min-h-screen flex items-center justify-center">
       <div className="max-w-6xl w-full flex flex-col md:grid md:grid-cols-2 md:mx-0 mx-4">
         {/* Left Side: Text */}
         <div className="flex flex-col justify-center">
           <h1 className="text-4xl md:text-5xl font-extrabold leading-none uppercase">
-            hi, i am <br />
-            <span className="text-white">damith mendis.</span>
+            hi, i am
+            <TypewriterEffectSmooth mainwords={HeroMainWords} />
           </h1>
           <div className="mt-4 text-gray-400 max-w-md">
             <span>Senior 3D Generalist | </span>
-            <FlipWords words={words} />
+            <FlipWords words={HeroFlipWords} />
           </div>
 
           {/* Buttons */}
@@ -29,40 +29,22 @@ export default function HeroSection() {
             >
               CONTACT ME
             </button>
-            <a
-              href="https://www.linkedin.com/in/damiya00"
-              aria-label="LinkedIn"
-              title="LinkedIn"
-              className="w-10 h-10 bg-white/10 text-white rounded-full flex items-center justify-center hover:bg-white/20 transition"
-            >
-              <i className="ri-linkedin-fill text-2xl text-[#D3E97A]" />
-            </a>
-            <a
-              href="https://www.artstation.com/damiya00"
-              aria-label="Art Station"
-              title="Art Station"
-              className="w-10 h-10 bg-white/10 text-white rounded-full flex items-center justify-center hover:bg-white/20 transition"
-            >
-              <Image
-                src="/icons/artstation-brands-solid.svg"
-                alt="Art Station"
-                width={24}
-                height={24}
-                className="w-5 h-5"
-              />
-            </a>
-            <a
-              href="https://www.behance.net/damithmendis"
-              aria-label="Behance"
-              title="Behance"
-              className="w-10 h-10 bg-white/10 text-white rounded-full flex items-center justify-center hover:bg-white/20 transition"
-            >
-              <i className="ri-behance-fill text-2xl text-[#D3E97A]" />
-            </a>
+
+            {HeroLinks.map((link, i) => (
+              <a
+                key={i}
+                href={link.href}
+                aria-label={link.label}
+                title={link.label}
+                className="w-10 h-10 bg-white/10 text-white rounded-full flex items-center justify-center hover:bg-white/20 transition"
+              >
+                <i className={`${link.iconClass} text-2xl text-[#D3E97A]`} />
+              </a>
+            ))}
           </div>
         </div>
 
-        {/* Right Side: Image with Background Gradient */}
+        {/* Right Side: Image */}
         <div className="flex justify-center items-center py-20 md:mt-0 md:py-0">
           <BackgroundGradient className="leading-none rounded-[22px] bg-white dark:bg-zinc-900">
             <Image
