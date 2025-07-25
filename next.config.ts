5dc45fc3d9d0c35debc8c4bba524767205e16a4a
dsamithmendis <samithmendis.01@gmail.com>
2025-07-25 22:18:30 +0530

    feat: deploy test one

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/www.damithmendis.com",
+  assetPrefix: "/www.damithmendis.com",
 };
 
 export default nextConfig;
