7e6937b4ab7a7ca8a595cfd8a30e167d25ad85bc
Samith Mendis <samithmendis.01@gmail.com>
2025-07-25 22:30:44 +0530

    Update route.ts

--- src/app/api/notsystem/route.ts
+++ src/app/api/notsystem/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from 'next/server';
 
+export const dynamic = "force-static";
+// OR: export const revalidate = 60;
+
 export async function GET() {
   return NextResponse.json({ message: 'Hello from notsystem route!' });
 }
