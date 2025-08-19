3ca45b0eda63c3f030ea7de1827956685d234b01
dsamithmendis <samithmendis.01@gmail.com>
2025-08-19 12:59:07 +0530

    feat: final touch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import "@/styles/global.css";
+import { Metadata } from "next";
+import Favicon from "@/../public//icons/favicon.ico";
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export const metadata: Metadata = {
+  title: {
+    default: "Portfolio | Damith Mendis",
+    template: "%s | Damith Mendis",
+  },
+  icons: [{ rel: "icon", url: Favicon.src }],
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
