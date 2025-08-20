327c4cc2a33c5c6c357f6794e9e3603409b93c16
dsamithmendis <samithmendis.01@gmail.com>
2025-08-20 20:27:16 +0530

    fix: change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,27 @@
 import "@/styles/global.css";
 import { Metadata } from "next";
-import Favicon from "@/../public//icons/favicon.ico";
+import Favicon from "@/../public/icons/favicon.ico";
+import { Macondo } from "next/font/google";
+
+const macondo = Macondo({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export const metadata: Metadata = {
   title: {
     default: "Portfolio | Damith Mendis",
     template: "%s | Damith Mendis",
   },
-  icons: [{ rel: "icon", url: Favicon.src }],
+  icons: {
+    icon: Favicon.src,
+  },
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={macondo.className}>{children}</body>
     </html>
   );
 }
