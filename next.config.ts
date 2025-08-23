ec69571161d544dfbd65c57d811e772a96bed977
dsamithmendis <samithmendis.01@gmail.com>
2025-08-23 05:43:18 +0530

    fix: build on netlify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const repo = "www.damithmendis.com";
-
 const deployTarget = process.env.NEXT_PUBLIC_DEPLOY_TARGET || "netlify";
 
 const isGitHub = deployTarget === "github";
@@ -11,9 +10,7 @@ const assetPrefix = isGitHub ? `/${repo}/` : "";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: isGitHub ? "export" : undefined,
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
   basePath,
   assetPrefix,
   env: {
