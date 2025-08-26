04a4608beee22ea7ded5a7a058d20c421a22f2cf
Samith Mendis <samithmendis.01@gmail.com>
2025-08-26 19:29:10 +0530

    fix: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
 import type { NextConfig } from "next";
 
-const repo = "www.damithmendis.com";
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+const assetPrefix = basePath ? `${basePath}/` : "";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}`,
+  images: { unoptimized: true },
+  basePath,
+  assetPrefix,
   env: {
-    NEXT_PUBLIC_BASE_PATH: `/${repo}`,
+    NEXT_PUBLIC_BASE_PATH: basePath,
   },
 };
 
